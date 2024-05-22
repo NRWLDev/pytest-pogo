2ba1fdffce2803c5a724cb0c10028728e690d428
Daniel Edgecombe <edgy.edgemond@gmail.com>
2024-05-22 10:55:02 +0100

    feat: Quiet down logs in pogo_engine fixture, and introduce pogo_engine_verbose with apply/rollback logs.

--- pytest_pogo/__init__.py
+++ pytest_pogo/__init__.py
@@ -1,9 +1,18 @@
-import asyncio
+import logging
+from contextlib import contextmanager
 
 import pytest
 
 import pogo_migrate.config
 import pogo_migrate.testing
+
+
+@contextmanager
+def caplog_session(request: pytest.FixtureRequest):  # noqa: ANN201
+    request.node.add_report_section = lambda *args: None  # noqa: ARG005
+    logging_plugin = request.config.pluginmanager.getplugin("logging-plugin")
+    for _ in logging_plugin.pytest_runtest_setup(request.node):
+        yield pytest.LogCaptureFixture(request.node, _ispytest=True)
 
 
 @pytest.fixture()
@@ -12,10 +21,22 @@ def pogo_config() -> pogo_migrate.config.Config:
 
 
 @pytest.fixture(scope="session")
-def pogo_engine():  # noqa: PT004, ANN201
-    asyncio.run(pogo_migrate.testing.apply())
+async def pogo_engine(request: pytest.FixtureRequest):  # noqa: PT004, ANN201
+    with caplog_session(request) as caplog, caplog.at_level(logging.ERROR):
+        await pogo_migrate.testing.apply()
 
     yield
 
     # Rollback all migrations
-    asyncio.run(pogo_migrate.testing.rollback())
+    with caplog_session(request) as caplog, caplog.at_level(logging.ERROR):
+        await pogo_migrate.testing.rollback()
+
+
+@pytest.fixture(scope="session")
+async def pogo_engine_verbose():  # noqa: PT004, ANN201
+    await pogo_migrate.testing.apply()
+
+    yield
+
+    # Rollback all migrations
+    await pogo_migrate.testing.rollback()
